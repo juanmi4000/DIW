4083a62a15eda0455793bcec18a2cf26f3b0797e
Juan Manuel <juanmi_4000@hotmail.es>
2024-02-28 20:45:41 +0100

    Falta dos secciones

--- ProyectoTailwind/tailwind.config.js
+++ ProyectoTailwind/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./fuente/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        comfortaa : ['Comfortaa', 'sans-serif'],
+        galada : ['Galada', 'Verdana', 'Geneva', 'Tahoma', 'sans-serif']
+      },
+      colors: {
+        colores: {
+          'header' : '#526A40',
+          'footer' : 'rgba(86, 58, 29, .9)',
+          'btn-reserva' : '#11150D',
+          'seccion-secundaria' : 'rgba(181, 234, 140, .4)' 
+        }
+      },
+      screens: {
+        'tableta' : '450px',
+        'ordenador' : '950px',
+      }
+    },
   },
   plugins: [],
 }
